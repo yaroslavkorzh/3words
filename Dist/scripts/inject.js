1fc3fe1a60f7d053b5e149cdb6f5d82441fea3a4
George Goncharenko <george.goncharenko@trustsourcing.com>
2016-05-17 10:38:15 +0300

    adjust popups

--- Dist/scripts/inject.js
+++ Dist/scripts/inject.js
@@ -116,17 +116,30 @@ function menuConstructor() {
         $(document).on("click.tsWordsPlugin", '.__ts-word', function (e) {
             e.stopPropagation();
             if($(this).hasClass('__ts-word--active')){
-                $('.__ts-word').removeClass('__ts-word--active');
+                if(!$(this).find('.__ts-word-tooltip').hasClass('__ts-word-tooltip--extended')){
+                    $(this).find('.__ts-word-tooltip').addClass('__ts-word-tooltip--extended');
+                    adjustPosition($(this));
+                }
+                else{
+                    $('.__ts-word').removeClass('__ts-word--active');
+                    $('.__ts-word-tooltip').removeClass('__ts-word-tooltip--extended');
+                    adjustPosition($(this));
+                }
+
                 self.isReady = true;
             }
             else{
                 $('.__ts-word').removeClass('__ts-word--active');
-                if(!$(this).hasClass('__ts-word--current-state')){
+                if(!$(this).hasClass('__ts-word--current-state') || true){
                     $(this).off(animationEnd);
                     $(this).removeClass('__ts-word--current-state animated jello').addClass('__ts-word--active');
                     if(!$(this).hasClass('__ts-word--learned-state--done')){
                         $(this).removeClass('__ts-word--learned-state').addClass('__ts-word--active-state');
                     }
+                    var $tooltip = $(this).find('.__ts-word-tooltip');
+                    $tooltip.addClass('__ts-word-tooltip--extended');
+
+                    adjustPosition($(this));
                     var id = $(this).data('id');
                     var data = self.getWordById(id);
                     data.actionCount +=1;
@@ -158,9 +171,13 @@ function menuConstructor() {
                 }
                 else{
                     $('.__ts-word').removeClass('__ts-word--active');
-                    if(!$el.hasClass('__ts-word--current-state')){
+                    if(!$el.hasClass('__ts-word--current-state' || true)){
                         $el.off(animationEnd);
                         $el.removeClass('__ts-word--current-state animated jello').addClass('__ts-word--active');
+                        if(!$el.hasClass('__ts-word--learned-state--done')){
+                            $el.removeClass('__ts-word--learned-state').addClass('__ts-word--active-state');
+                        }
+                        adjustPosition($el);
                         var id = $el.data('id');
                         var data = self.getWordById(id);
                         data.actionCount +=1;
@@ -199,10 +216,12 @@ function menuConstructor() {
             e.stopPropagation();
             var $tooltip = $(this).parents('.__ts-word-tooltip');
             $tooltip.addClass('__ts-word-tooltip--extended');
+            adjustPosition($(this).parents('.__ts-word'));
         });
         $(document).on("click.tsWordsPlugin", function (e) {
-            $('.__ts-word').removeClass('__ts-word--active');
+            $('.__ts-word').removeClass('__ts-word--active').removeClass('__ts-pos-left').removeClass('__ts-pos-right');
             $('.__ts-word-tooltip').removeClass('__ts-word-tooltip--extended');
+
         });
 
     };
@@ -438,11 +457,11 @@ function menuConstructor() {
                         '<ts3Words class="__ts-word-tooltip__header">'+data.word+' - '+data.translation+'</ts3Words>' +
                         '<ts3Words class="__ts-word-tooltip__description">' +
                             '<ts3Words class="__ts-word-tooltip__blockquote">' +
-                                '<ts3Words class="__ts-word-tooltip__blockquote__spelling">Pronounced in Russian: <ts3Words class="__ts-word-tooltip__blockquote__spelling-word">'+data.word+'</ts3Words><ts3Words class="__ts-icon __ts-icon--volume"></ts3Words></ts3Words>' +
+                                '<ts3Words class="__ts-word-tooltip__blockquote__spelling">Pronounced in Russian: <ts3Words class="__ts-word-tooltip__blockquote__spelling-word">'+data.word+'</ts3Words><ts3Words class="__ts-icon __ts-icon--volume"><svg viewBox="0 0 31.237 31.237"><path d="M4.363,10.761H0v9.715h4.363l8.244,6.114c0,0,1.512,1.268,1.512-0.041c0-1.312,0-20.858,0-22.051 c0-1.028-1.33-0.024-1.33-0.024L4.363,10.761z"/><path d="M18.816,8.865c-0.436-0.434-1.137-0.434-1.568,0c-0.436,0.434-0.436,1.138,0,1.568 c1.426,1.431,2.133,3.286,2.135,5.157c-0.002,1.875-0.709,3.742-2.135,5.166c-0.436,0.434-0.436,1.136,0,1.573 c0.215,0.218,0.498,0.324,0.785,0.324c0.281,0,0.566-0.106,0.783-0.324c1.855-1.859,2.787-4.306,2.785-6.739 C21.604,13.155,20.672,10.721,18.816,8.865z"/><path d="M21.824,5.37c-0.438-0.436-1.139-0.436-1.572,0c-0.43,0.435-0.43,1.137,0,1.572 c2.398,2.396,3.592,5.52,3.596,8.657c-0.004,3.154-1.193,6.292-3.596,8.702c-0.432,0.434-0.43,1.133,0,1.57 c0.219,0.212,0.502,0.322,0.787,0.322c0.283,0,0.568-0.11,0.785-0.322c2.832-2.84,4.242-6.562,4.242-10.272 C26.066,11.901,24.645,8.194,21.824,5.37z"/> <path d="M25.508,1.79c-0.432-0.436-1.135-0.436-1.57,0c-0.43,0.434-0.43,1.138,0,1.568 c3.387,3.387,5.078,7.809,5.078,12.245c0,4.449-1.686,8.884-5.078,12.278c-0.434,0.433-0.43,1.139,0,1.57 c0.219,0.215,0.504,0.322,0.789,0.322c0.281,0,0.564-0.107,0.781-0.322c3.822-3.825,5.73-8.845,5.729-13.849 C31.238,10.607,29.32,5.602,25.508,1.79z"/></svg> </ts3Words></ts3Words>' +
                                 '<ts3Words class="__ts-word-tooltip__blockquote__spelling">Written: <ts3Words class="__ts-word-tooltip__blockquote__spelling-word">'+data.transliteration+'</ts3Words></ts3Words>' +
                             '</ts3Words>' +
                             '<ts3Words class="__ts-word-tooltip__example">' +
-                                '<ts3Words class="__ts-word-tooltip__example__title"><ts3Words class="__ts-icon __ts-icon--info"></ts3Words> Examples of usage:</ts3Words>' +
+                                '<ts3Words class="__ts-word-tooltip__example__title"><ts3Words class="__ts-icon __ts-icon--info __ts-icon--info--example"><svg viewBox="0 0 543.906 543.906"><path  d="M271.953,0C121.759,0,0,121.759,0,271.953s121.759,271.953,271.953,271.953 s271.953-121.759,271.953-271.953S422.148,0,271.953,0z M316.994,76.316c1.055-0.049,2.138-0.06,3.231,0 c14.724-0.484,27.533,10.622,29.578,24.987c6.576,27.581-22.719,55.228-49.631,44.192 C268.032,130.576,284.224,77.909,316.994,76.316z M303.739,196.318c20.875-1.327,24.519,22.964,18.014,47.592 c-12.695,56.583-32.455,111.403-43.175,168.442c5.178,22.523,33.575-3.312,45.721-11.558c10.329-8.213,12.124,2.083,15.637,10.71 c-25.776,18.058-51.687,36.447-80.395,50.991c-26.972,16.361-49.049-9.072-42.321-37.394 c11.128-52.841,25.776-104.882,37.736-157.564c3.737-28.468-33.728,0.511-44.872,7.136c-8.985,11.292-13.25,3.051-16.997-7.136 c29.871-21.816,60.325-48.593,93.313-65.949C293.138,198.238,298.92,196.622,303.739,196.318z"/></svg></ts3Words> Examples of usage:</ts3Words>' +
                                 '<ts3Words class="__ts-word-tooltip__example__text">'+data.example+'</ts3Words>' +
                             '</ts3Words>' +
                         '</ts3Words>' +
@@ -788,4 +807,22 @@ function elementInViewport(el) {
         return false;
     }
 
+}
+
+
+function adjustPosition($el){
+    $el.removeClass('__ts-pos-left').removeClass('__ts-pos-right');
+    var left = $el.find('.__ts-word-tooltip').offset().left;
+    var space = 20;
+    if(left  < space){
+        $el.removeClass('__ts-pos-right').addClass('__ts-pos-left');
+    }
+    else{
+        if(($(window).width() - ($el.find('.__ts-word-tooltip').offset().left + $el.find('.__ts-word-tooltip').width())) < space){
+            $el.removeClass('__ts-pos-left').addClass('__ts-pos-right');
+        }
+        else{
+            $el.removeClass('__ts-pos-left').removeClass('__ts-pos-right');
+        }
+    }
 }
